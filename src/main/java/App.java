b1b38c30f68099be091f435e97521934cbc3fdf7
nastya199498 <114832643+nastya199498@users.noreply.github.com>
2023-01-30 13:44:03 +0300

    zadacha2.1.3

---
+++ src/main/java/App.java
@@ -0,0 +1,23 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+        Cat catBean =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getCat());
+        Cat catBean1 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean1.getCat());
+        System.out.println("Равны ли bean:" + (bean==bean1));
+        System.out.println("Равны ли catBean:" + (catBean==catBean1));
+    }
+}
---
+++ src/main/java/Cat.java
@@ -0,0 +1,12 @@
+
+public class Cat {
+private String cat;
+
+    public String getCat() {
+        return cat;
+    }
+
+    public void setCat(String cat) {
+        this.cat = cat;
+    }
+}
